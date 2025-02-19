df79df8204420d2a20d36a31140ca62d296f24df
hampusvh <hampus.vh@gmail.com>
2025-02-19 22:40:17 +0100

    removed prompt, added editing mode

--- note-taking-app/app-project/src/components/NoteCard.jsx
+++ note-taking-app/app-project/src/components/NoteCard.jsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
 import EditButton from "./EditButton";
 import DeleteButton from "./DeleteButton";
 
 function NoteCard({ note, index, editNote, deleteNote }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [newText, setNewText] = useState(note);
+
+    const handleSave = () => {
+        editNote(index, newText);
+        setIsEditing(false);
+    };
+
   return (
     <div className="note-box">
-      <p>{note}</p>
+      {isEditing ? (
       <div>
-        <EditButton
-          editNote={() => editNote(index, prompt("Edit note:", note))}
+        <input
+        type="text"
+        value={newText}
+        onChange={(e) => setNewText(e.target.value)}
+        className="edit-input"
         />
-        <DeleteButton deleteNote={() => deleteNote(index)} />
-      </div>
-    </div>
+        <button className="save-btn" onClick={handleSave}>💾 Save</button>
+        </div>
+      ) : (
+        <div>
+            <p>{note}</p>
+            <div>
+                <EditButton editNote={() => setIsEditing(true)} />
+                <DeleteButton deleteNote={() => deleteNote(index)} />
+            </div>
+        </div>
+  )}
+  </div>
   );
 }
 
