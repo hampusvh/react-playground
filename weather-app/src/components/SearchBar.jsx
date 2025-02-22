a3abccdafb08a02b1307066d4b2568ce42d85e5e
hampusvh <hampus.vh@gmail.com>
2025-02-22 12:10:24 +0100

    SearchBar.jsx attempt

---
+++ weather-app/src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+
+
+function SearchBar ({ SearchBar }) {
+
+    return (
+        <div>
+            <input type="text"
+            placeholder="search">
+            
+            <button
+            onClick={search}>Search</button>
+            </input>
+        </div>
+    );
+
+}
+
+export detfault SearchBar;
